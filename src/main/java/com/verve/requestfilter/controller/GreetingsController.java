edd4c98ddee2c3d25fdf3c2daa41c4a28de1b974
Naveen Kumar Errojula <naveenkumarerroju@gmail.com>
2025-03-08 15:25:01 +0530

    high level architecture

--- src/main/java/com/verve/requestfilter/controller/GreetingsController.java
+++ src/main/java/com/verve/requestfilter/controller/GreetingsController.java
@@ -3,11 +3,13 @@ package com.verve.requestfilter.controller;
 import com.verve.requestfilter.service.GreetingsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/api/verve")
 public class GreetingsController {
 
     private final GreetingsService service;
@@ -17,7 +19,7 @@ public class GreetingsController {
         this.service = service;
     }
 
-    @GetMapping("/api/verve/accept")
+    @GetMapping("/accept")
     public Mono<String> acceptRequest(
             @RequestParam int id,
             @RequestParam(required = false) String endpoint) {
